81cfd6f84752d1b0ff856653f4a0f3c7ca97cb4c
Ferrone84 <nico.duret@orange.fr>
2018-03-05 11:08:09 +0100

    Init

---
+++ app/src/main/java/com/example/duret/iceandroid/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.duret.iceandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bonsouar;
+    private TextView hw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bonsouar = findViewById(R.id.bonsouar);
+        hw = findViewById(R.id.hw);
+
+        bonsouar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hw.setText("Bonsouar");
+            }
+        });
+    }
+}
